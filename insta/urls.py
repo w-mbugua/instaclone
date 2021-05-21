72b482d8480d02d259439127821a7c513456f14a
JoyMbugua <wacumbugua@gmail.com>
2021-05-21 13:17:43 +0300

    added django-registration page

--- insta/urls.py
+++ insta/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
