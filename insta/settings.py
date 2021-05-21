0775a592135c9e079bf340ddc9eeb7906d1e9d5d
JoyMbugua <wacumbugua@gmail.com>
2021-05-21 15:28:46 +0300

    added authentication links to templates

--- insta/settings.py
+++ insta/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
