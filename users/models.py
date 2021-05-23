81f975d57afa0bf05aa503098202c8f169097fb6
JoyMbugua <wacumbugua@gmail.com>
2021-05-23 13:24:46 +0300

    added relationship model

--- users/models.py
+++ users/models.py
@@ -61,6 +61,22 @@ class Like(models.Model):
     def __str__(self):
         return f"{self.user}-{self.image}-{self.value}"
 
+STATUS_CHOICES = (
+('send', 'send'),
+('accepted', 'accepted')
+)
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
+
+
 
 
 
